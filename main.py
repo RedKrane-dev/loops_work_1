6e7c959eca5cc6e711b7cc2ca4ba9e42bace4352
RedKrane86 <redkrane86@gmail.com>
2026-02-03 22:41:59 +0500

    fixes, reform

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-user_input = input('Введите целое положительное число:\n')
-
-if user_input.isdigit() and int(user_input) > 0:
-    counter = int(user_input)
-    while counter + 1:
-        print(counter)
-        counter -= 1
+def take_and_check_input():
+    user_input = input('Введите целое положительное число:\n')
+    if user_input.isdigit() and int(user_input) > 0:
+        return int(user_input)
     else:
-        print('Программа завершена')
-else:
-    print('Некорректный ввод')
+        print('Некорректный ввод')
+
+def countdown(counter):
+    if counter:
+        while counter + 1:
+            print(counter)
+            counter -= 1
+        else:
+            print('Программа завершена')
+
+if __name__ == '__main__':
+    countdown(take_and_check_input())
